a42a25e65587da24fff5784e8bd195aa9d64a414
Luke0328 <curly0328@gmail.com>
2023-02-06 23:08:42 -0700

    Complete exercises 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, n) {
+    if(n < 0) {
+        return "ERROR";
+    }
+    let res = "";
+    for(let i = 0; i < n; i++) {
+        res += phrase;
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let rev = "";
+    for(let i = input.length - 1; i >= 0; i--) {
+        rev += input[i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
